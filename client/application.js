527c281e6145f536ee4473c6520c5c0c959cb957
Jack Hamilton & John Craigie & Norberto Caceres & Kevin Sullivan <pair+jacklh9.jcraigie.norberto94.wolverineks@devbootcamp.com>
2016-04-24 16:15:09 -0700

    Move views from server to client folder, along with js and css folders

---
+++ client/application.js
@@ -0,0 +1,32 @@
+$(function(){
+  console.log("happy")
+
+  $('#new-entrant-form').submit(function(event){
+    event.preventDefault();
+    console.log("happy happy")
+
+
+
+
+
+
+  })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
--- server/server.js
+++ server/server.js
@@ -16,7 +16,8 @@ app.set('json spaces', 2); // format json responses for easier viewing
 
 // must be set to serve views properly when starting the app via `slc run` from
 // the project root
-app.set('views', path.resolve(__dirname, 'views'));
+app.set('views', path.resolve(__dirname, '../client'));
+// app.set('views', path.resolve(__dirname, 'views'));
 
 app.start = function() {
   // start the web server
