8c66d29592c94764cc93c74d513defff17459002
Jack Hamilton & John Craigie & Norberto Caceres & Kevin Sullivan <pair+jacklh9.jcraigie.norberto94.wolverineks@devbootcamp.com>
2016-04-24 16:38:06 -0700

    Add entrants to raffle

--- client/application.js
+++ client/application.js
@@ -5,12 +5,20 @@ $(function(){
     event.preventDefault();
     console.log("happy happy")
 
+    var url = $(this).attr('action');
+    var data = $(this).serialize();
 
-
-
-
-
-  })
+    $.ajax({
+      method: "POST",
+      url: url,
+      data: data
+    })
+      .done(function(response){
+        console.log(response);
+        $('#entrants-list').prepend("<li>" + response.username + "</li>");
+        $('#new-entrant-form').trigger('reset');
+      });
+  });
 
 
 
