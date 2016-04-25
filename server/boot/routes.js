b550447d6a20582cf77fb8667aaf9a6fb653b4f5
Jack L. Hamilton <jacklh@gmail.com>
2016-04-25 04:32:26 -0700

    Move logic from router to model for show page, raffle/id/run and raffle/id

--- server/boot/routes.js
+++ server/boot/routes.js
@@ -32,72 +32,16 @@ module.exports = function(app) {
   router.get(/raffles\/\d+\/run/, function(req, res) {
     var re = /raffles\/(\d+)\/run/;
     var id = req.url.match(re)[1];
-    console.log("Raffle id: " + id);
-
     var Raffle = app.models.raffle;
-    var Entrant = app.models.entrant;
-    var raffle = null;
-    var eligible_entrants = null;
-    var winner = null;
-    var previous_winners = null;
-    var alert = null;
-
-
-    var render_round = function() {
-      console.log("Inside render_round");
-      res.render('show', {
-        raffle: raffle,
-        alert: alert,
-        entrants: eligible_entrants,
-        winner: winner,
-        previous_winners: previous_winners
-      });
-    };
-
-    var get_eligible_entrants = function(result){
-      console.log("Inside get_eligible_entrants");
-      raffle = result;
-      raffle.updateAttribute('active', true);
-
-      Entrant.find({where: {and: [{raffleId: raffle.id}, {eligible: true}]}}).then(get_previous_winners);
-    };
-
-    var get_previous_winners = function(result){
-      console.log("Inside get_previous_winners");
-      eligible_entrants = (result && result.length > 0) ? result : null;
-      Entrant.find({where: {and: [{raffleId: raffle.id}, {eligible: false}]}}).then(get_winner);
-    };
-
-    var get_winner = function(result){
-      console.log("Inside get_winner");
-      previous_winners = (result && result.length > 0) ? result : null;
-
-      if (eligible_entrants && eligible_entrants.length > 0){
-        var winner_index = Math.floor(Math.random() * eligible_entrants.length);
-        winner = eligible_entrants[winner_index];
-        console.log("Winner index: " + winner_index);
-        console.log("Winner name: " + winner.username);
-        winner.updateAttribute('eligible', false);
-        alert = "" + winner.username + " has won this round!!!"
-      } else {
-        raffle.updateAttribute('active', false);
-        alert = "Raffle is over."
-      }
-
-      render_round();
-    };
-
-    console.log("Before Raffle findById");
-    Raffle.findById(id).then(get_eligible_entrants);
-    console.log("After Raffle findById");
+    Raffle.render_raffle(id, res);
   });
 
-  router.delete(/raffles\/\d+\/run/, function(req, res) {
+  router.delete(/raffles\/\d+/, function(req, res) {
     var re = /raffles\/(\d+)\/run/;
     var id = req.url.match(re)[1];
 
     raffle.updateAttribute('active', false);
-    res.redirect('/raffles');
+    Raffle.render_raffle(id, res);
   });
 
 
