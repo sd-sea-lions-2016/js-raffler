c33e491b2538851ec8d30dba0a82d1e7e99b208e
Jack Hamilton <email": null>
2016-04-25 16:53:32 -0700

    Remove excessive console logs during debugging sessions

--- common/models/raffle.js
+++ common/models/raffle.js
@@ -25,12 +25,6 @@ module.exports = function(Raffle) {
       var alert = null;
 
       var render_round = function(raffle) {
-        console.log("Inside render_round");
-        console.log("raffle.id: " + raffle.id);
-        console.log("alert: " + alert);
-        console.log("eligible_entrants: " + eligible_entrants);
-        console.log("winner: " + winner);
-        console.log("previous_winners: " + previous_winners);
 
         res.render('show', {
           raffle: raffle,
@@ -43,8 +37,6 @@ module.exports = function(Raffle) {
 
       console.log("Round id: " + id);
       Raffle.findById(id).then(function(raffle){
-        console.log("Inside findByID.then");
-        console.log("raffle id: " + raffle.id);
         entrants = raffle.entrants();
 
         eligible_entrants = entrants.filter(function(entrant){
@@ -58,25 +50,11 @@ module.exports = function(Raffle) {
         if (raffle.active && eligible_entrants && eligible_entrants.length > 0){
           var winner_index = Math.floor(Math.random() * eligible_entrants.length);
           winner = eligible_entrants[winner_index];
-          console.log("Winner index: " + winner_index);
-          console.log("Winner name: " + winner.username);
           // update entrant to be ineligible (already won once) from further rounds in this raffle.
-          console.log(raffle.entrants.at(winner_index));
-          console.log("About to update entrant active attribute to false...");
-          console.log(raffle);
           raffle.entrants.updateById(winner.id, function(err, entrant){
-            console.log("Inside updateById");
-            console.log(entrant)
-            entrant.active = false;
             entrant.updateAttribute('eligible', false);
-            console.log(entrant);
-            console.log(raffle);
             entrant.save();
           });
-
-          console.log("...done. Outside updateById to see if persists...");
-          console.log(raffle);
-
           alert = "" + winner.username + " has won this round!!!";
         } else {
           raffle.updateAttribute('active', false);
