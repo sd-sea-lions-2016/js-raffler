5b92de5dd7b1e2d148cb2b486867f2c0ce897558
Norberto Caceres <norbe94@hotmail.com>
2016-04-27 11:02:28 -0700

    edit plinko to fit screen and logic changes

--- client/plinko.js
+++ client/plinko.js
@@ -1,27 +1,39 @@
 $( document ).ready(function() {
-      console.log($('#numContestants').html());
+      // console.log($('#numContestants').html());
       var numContestants = $('#numContestants').html();
-      console.log("Inside plinko");
-      console.log("numContestants: " + numContestants + "!");
+      // console.log("Inside plinko");
+      // console.log("numContestants: " + numContestants + "!");
       var row = 1;
       var previousRow = 0;
       var nextRow = 2
       var startingSquare = Math.floor(Math.random() * (numContestants*2));
       var square = startingSquare + 1;
       var previousSquare = square;
-      var falling_ball_timeout = 110; // ms
-      var num_rows = 25;
+      var falling_ball_timeout = 100; // ms
+      var num_rows = 21;
       var extra = 2;
       var wait_time_before_winner_announced = falling_ball_timeout * (num_rows + extra); // ms
       var i = 0;
-      console.log(square);
+
+      // console.log(square);
       (function move (i) {
         setTimeout(function () {
           $('table :first-child :nth-child('+row+') :nth-child('+square+')').addClass('black');
           $('table :first-child :nth-child('+previousRow+') :nth-child('+previousSquare+')').removeClass('black');
-          var whereToMove = Math.floor(Math.random() * (2));
-          var LEFT = 1;
-          var RIGHT = 0;
+          var whereToMove = Math.floor(Math.random() * (4));
+          var random = Math.floor(Math.random() * (20));
+          if(whereToMove == 2 || whereToMove == 3){
+            var whereToMove = [2,2,2,2,3,3,2,2,2,2,2,1,1,2,2,2,2,2,2,2][random];
+          }else{
+            var whereToMove = [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,3,3][random];
+          };
+
+          // console.log(whereToMove);
+
+          var TWO_LEFT = 1;
+          var TWO_RIGHT = 2;
+          var LEFT = 0;
+          var RIGHT = 3;
           var LEFT_MOST_SQUARE = 1;
           var RIGHT_MOST_SQUARE = (numContestants * 2)
 
@@ -43,6 +55,18 @@ $( document ).ready(function() {
                 }
                 else if(whereToMove == RIGHT && square == RIGHT_MOST_SQUARE){
                   square--;
+                }
+                else if(whereToMove == TWO_RIGHT && square != RIGHT_MOST_SQUARE){
+                  square++
+                }
+                 else if(whereToMove == TWO_LEFT && square != LEFT_MOST_SQUARE){
+                  square--;
+                }
+                else if (whereToMove == TWO_RIGHT && square == RIGHT_MOST_SQUARE){
+                  square--;
+                }
+                else if(whereToMove == TWO_LEFT && square == LEFT_MOST_SQUARE){
+                  square++;
                 }
               }
               else{
