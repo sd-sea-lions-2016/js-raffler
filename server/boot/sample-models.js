15a8c157a0e38015a24ab48d221b4e126ddaa75b
wolv <wolverine.ks@gmail.com>
2016-04-27 10:44:23 -0700

    Add conditional to seed

--- server/boot/sample-models.js
+++ server/boot/sample-models.js
@@ -1,41 +1,45 @@
-// module.exports = function(app) {
-//   var User = app.models.user;
-//   var Role = app.models.Role;
-//   var RoleMapping = app.models.RoleMapping;
-//
-//   User.create([
-//     {username: 'Admin', email: 'admin@sdjs-raffle.com', password: 'sdjspassword'}
-//   ], function(err, users) {
-//     if (err) throw err;
-//
-//     console.log('Created users:', users);
-//
-//     // create raffle 1 and make john the owner
-//     users[0].raffles.create({
-//       date: new Date(),
-//       active: false
-//     }, function(err, raffle) {
-//       if (err) throw err;
-//       console.log('Created raffle:', raffle);
-//     });
-//
-//     //create the admin role
-//     Role.create({
-//       name: 'admin'
-//     }, function(err, role) {
-//       if (err) throw err;
-//
-//       console.log('Created role:', role);
-//
-//       //make bob an admin
-//       role.principals.create({
-//         principalType: RoleMapping.USER,
-//         principalId: users[0].id
-//       }, function(err, principal) {
-//         if (err) throw err;
-//
-//         console.log('Created principal:', principal);
-//       });
-//     });
-//   });
-// };
+module.exports = function(app) {
+  var User = app.models.user;
+  var Role = app.models.Role;
+  var RoleMapping = app.models.RoleMapping;
+
+  User.find({username: 'Admin'}).then(result){
+    if (!result) {
+      User.create([
+        {username: 'Admin', email: 'admin@sdjs-raffle.com', password: 'sdjspassword'}
+      ], function(err, users) {
+        if (err) throw err;
+
+        console.log('Created users:', users);
+
+        // create raffle 1 and make john the owner
+        users[0].raffles.create({
+          date: new Date(),
+          active: false
+        }, function(err, raffle) {
+          if (err) throw err;
+          console.log('Created raffle:', raffle);
+        });
+
+        //create the admin role
+        Role.create({
+          name: 'admin'
+        }, function(err, role) {
+          if (err) throw err;
+
+          console.log('Created role:', role);
+
+          //make bob an admin
+          role.principals.create({
+            principalType: RoleMapping.USER,
+            principalId: users[0].id
+          }, function(err, principal) {
+            if (err) throw err;
+
+            console.log('Created principal:', principal);
+          });
+        });
+      });
+    } // end if statement
+  };
+};
