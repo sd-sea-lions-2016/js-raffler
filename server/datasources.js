54679569dbfc6fa9f5f160c1b293ff8b994ebdca
wolv <wolverine.ks@gmail.com>
2016-04-26 21:48:49 -0700

    Add correct environmental variables

--- server/datasources.js
+++ server/datasources.js
@@ -1,9 +1,11 @@
 module.export = {
-  host:      ds013911.mlab.com,
-  port:      13911,
-  database:  heroku_mdsgp3sf,
-  password:  process.env.DB_PASSWORD,
-  name:      sdjs-raffle,
-  user:      heroku_mdsgp3sf,
-  connector: mongodb
+  raffle-js = {
+    host:      ds013911.mlab.com,
+    port:      13911,
+    database:  heroku_mdsgp3sf,
+    password:  process.env.DB_PASSWORD,
+    name:      heroku_mdsgp3sf,
+    user:      heroku_mdsgp3sf,
+    connector: mongodb
+  }
 }
