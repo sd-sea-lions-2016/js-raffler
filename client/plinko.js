340c99a5bf5152fbf3d048d5da72d25c57a40d38
Jack L. Hamilton <jacklh@gmail.com>
2016-04-27 18:56:20 -0700

    Set up put to db

--- client/plinko.js
+++ client/plinko.js
@@ -1,18 +1,91 @@
-$(function() {
-  new Plinko();
+$(function() {  
+  console.log("Inside plinko");
+
+  // RaffleConnector returns an array of contestants
+  var bob = "bob was here";
+  var rc = new RaffleConnector(function(contestants){
+    p = new Plinko();
+    p.run(contestants, function(winner){
+      console.log("Inside plinko cb");
+      console.log("Winner is " + winner.username);
+      console.log(winner.id);
+      // update DB
+      console.log(bob);
+      rc.setWinner(winner);
+    });
+  });
+
 });
 
-function Plinko(){
-  this.loadRaffle();
-  this.run();
+function RaffleConnector(cb){
+  console.log("Inside RaffleConnector");
+  this.contestants = null;
+  this.numContestants = null;
+  this.raffle_path_with_id = null;
+  this.init(cb);
+}; // end RaffleConnector()
+
+
+RaffleConnector.prototype.init = function(cb){
+  console.log("Getting contestants from database");
+
+  this.raffle_path_with_id = $('#start-raffle-round').attr('action');
+  console.log("url: " + this.raffle_path_with_id);
+
+  request_url = window.location.protocol + "//" + window.location.host + "/api" + this.raffle_path_with_id + "/entrants";
+  console.log("new url: " + request_url);
+
+  $.ajax({
+    method: 'GET',
+    url: request_url
+  }) 
+    .done(function(response){
+      console.log("Return from ajax GET call");
+      console.log(response);
+      console.log("Single entrant: " + response[0]);
+      this.contestants = response;
+      cb(response);
+  })
+    .fail(function(err){
+      alert("DB error: " + err);
+    });
+}; // end init()
+
+RaffleConnector.prototype.setWinner = function(winner){
+  var data = {"eligible": false, "id": winner.id }
+  var url = window.location.protocol + "//" + window.location.host + "/api" + this.raffle_path_with_id + "/entrants/" + winner.id;
+  $.ajax({
+    method: 'PUT',
+    url: request_url,
+    data: data
+  }) 
+    .done(function(response){
+      console.log("Return from ajax PUT call after raffle win.");
+      console.log(response);
+  })
+    .fail(function(err){
+      alert("DB error: " + err);
+    });
 };
 
-Plinko.prototype.loadRaffle = function(){
-  this.numContestants = 10;
-};
 
-Plinko.prototype.move = function(){
-  setTimeout(function () {
+function Plinko(){};
+Plinko.prototype.run = function(contestants, cb) {
+  var numContestants = contestants.length;
+  var row = 1;
+  var previousRow = 0;
+  var nextRow = 2
+  var startingSquare = Math.floor(Math.random() * (numContestants*2));
+  var square = startingSquare + 1;
+  var previousSquare = square;
+  var falling_ball_timeout = 110; // ms
+  var num_rows = 25;
+  var extra = 5;
+  var wait_time_before_winner_announced = falling_ball_timeout * (num_rows + extra); // ms
+  var i = 0;
+  console.log(square);
+  (function move (i) {
+    setTimeout(function () {
       $('table :first-child :nth-child('+row+') :nth-child('+square+')').addClass('black');
       $('table :first-child :nth-child('+previousRow+') :nth-child('+previousSquare+')').removeClass('black');
       var whereToMove = Math.floor(Math.random() * (2));
@@ -45,38 +118,20 @@ Plinko.prototype.move = function(){
           square;
           }
       }
+      if (nextRow > num_rows){
+        setTimeout(checkForWinner(cb), wait_time_before_winner_announced);
+      }
     }, falling_ball_timeout);
-  }
-};
+  })(num_rows);
 
-Plinko.prototype.run = function(){
-  console.log(this.numContestants);
-  var numContestants = this.numContestants;
-  console.log("Inside plinko");
-  console.log("numContestants: " + numContestants + "!");
-  var row = 1;
-  var previousRow = 0;
-  var nextRow = 2
-  var startingSquare = Math.floor(Math.random() * (numContestants*2));
-  var square = startingSquare + 1;
-  var previousSquare = square;
-  var falling_ball_timeout = 110; // ms
-  var num_rows = 25;
-  var extra = 4;
-  var wait_time_before_winner_announced = falling_ball_timeout * (num_rows + extra); // ms
-  var i = 0;
-  console.log(square);
-  this.move(num_rows);
-
-  setTimeout(function(){
+  function checkForWinner(cb){
     var cellIndex = $('.black').index() / 2;
     $('.black').closest('tr').next().children().eq(cellIndex).css('background-color', 'red');
-    var winner = $('.black').closest('tr').next().children().eq(cellIndex).html();
-    alert(winner+' has won!');
-  }, wait_time_before_winner_announced);
+    var winner = {};
+    winner_td_tag = $('.black').closest('tr').next().children()
+    winner.username = winner_td_tag.eq(cellIndex).html();
+    winner.id = winner_td_tag.attr('id');
+    cb(winner);
+  };
 
 };
-
-
-
-
